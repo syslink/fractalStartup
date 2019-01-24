eb919d052d537b4e032a39aa71dcbac6fe6c4c98
syslink <oursyslink@163.com>
2019-01-24 20:27:33 +0800

    fix bug of createGenesisNode.sh

--- createGenesisNode.sh
+++ createGenesisNode.sh
@@ -9,11 +9,18 @@ do
     temp_file=`basename $file_a`
     if [[ $temp_file =~ ft ]]
     then
-        $ftExeFile=$temp_file
+        ftExeFile=$temp_file
         break
     fi   
 done
 
-ps x | grep $p2pPort | awk '{print $1}' | xargs kill >./datadir/null 2>&1
-./$ftExeFile --datadir ./datadir/$p2pPort --http_host 0.0.0.0 --p2p_listenaddr :$p2pPort --http_port $httpPort --ws_port $wsPort --http_api=keystore --http_api=miner --http_api=dpos --http_api=account --http_api=txpool --http_api=p2p --http_api=zip --miner_start
-tail -f ./datadir/$p2pPort.log | sed -n '/UDP listener up/p' | awk -F "=" '{print $2}' | sed 's/[::]/${localIp}/g' > nodes.txt
+mkdir -p ./datadir
+ps x | grep $p2pPort | awk '{print $1}' | xargs kill >./datadir/${p2pPort}null 2>&1
+sleep 3s
+echo $ftExeFile
+./$ftExeFile --datadir ./datadir/$p2pPort --http_host 0.0.0.0 --p2p_listenaddr :$p2pPort --http_port $httpPort --ws_port $wsPort --http_api="keystore,miner,dpos,account,txpool,p2p,ft" --miner_start > ./datadir/$p2pPort.log 2>&1 &
+echo 'start up genesis node over'
+ps x | grep $p2pPort | grep $ftExeFile
+sleep 3
+cat ./datadir/$p2pPort.log | sed -n '/UDP listener up/p' | awk -F "=" '{print $2}' | sed "s/\[::\]/$localIp/g" > nodes.txt 2>&1 &
+echo 'write node info over'
