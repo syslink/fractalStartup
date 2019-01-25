75f7765b62c851a627578f2640acf55bfd122490
syslink <oursyslink@163.com>
2019-01-25 11:06:06 +0800

    modify

--- createGenesisNode.sh
+++ createGenesisNode.sh
@@ -18,7 +18,7 @@ mkdir -p ./datadir
 ps x | grep $p2pPort | awk '{print $1}' | xargs kill >./datadir/${p2pPort}null 2>&1
 sleep 3s
 echo $ftExeFile
-./$ftExeFile --datadir ./datadir/$p2pPort --http_host 0.0.0.0 --p2p_listenaddr :$p2pPort --http_port $httpPort --ws_port $wsPort --http_api="keystore,miner,dpos,account,txpool,p2p,ft" --miner_start > ./datadir/$p2pPort.log 2>&1 &
+./$ftExeFile --genesis ./genesis.json --datadir ./datadir/$p2pPort --http_host 0.0.0.0 --p2p_listenaddr :$p2pPort --http_port $httpPort --ws_port $wsPort --http_api="keystore,miner,dpos,account,txpool,p2p,ft" --miner_start > ./datadir/$p2pPort.log 2>&1 &
 echo 'start up genesis node over'
 ps x | grep $p2pPort | grep $ftExeFile
 sleep 3
--- start.sh
+++ start.sh
@@ -21,15 +21,16 @@ do
     temp_file=`basename $file_a`
     if [[ $temp_file =~ ft ]]
     then
-        $ftExeFile=$temp_file
+        ftExeFile=$temp_file
         break
     fi   
 done
 
 while nodeNum > 0
 do
-    ps x | grep $p2pPort | awk '{print $1}' | xargs kill >./datadir/null 2>&1
-    ./$ftExeFile  --genesis ./genesis.json --datadir ./datadir/$p2pPort --p2p_maxpeers 21 --http_host 0.0.0.0  --p2p_bootnodes ./nodes.txt --http_port $httpPort --ws_port $wsPort --log_level 4 --http_api="miner,dpos,account,txpool,p2p,ft,keystore"  --miner_coinbase testtest5 --miner_private 8ee847ae5974a13ce9df66083e453ea1e0f7995379ed027a98e827aa8b6bc211 --miner_start > ./datadir/$p2pPort.log 2>&1 &
+    ps x | grep $p2pPort | awk '{print $1}' | xargs kill >./datadir/${p2pPort}null 2>&1
+    sleep 3
+    ./$ftExeFile  --datadir ./datadir/$p2pPort --p2p_maxpeers 21 --http_host 0.0.0.0  --p2p_bootnodes ./nodes.txt --http_port $httpPort --ws_port $wsPort --log_level 4 --http_api="miner,dpos,account,txpool,p2p,ft,keystore"  --miner_coinbase testtest5 --miner_private 8ee847ae5974a13ce9df66083e453ea1e0f7995379ed027a98e827aa8b6bc211 --miner_start > ./datadir/$p2pPort.log 2>&1 &
     nodeNum='expr $nodeNum-1'
     p2pPort='expr $p2pPort+1'
     httpPort='expr $httpPort+1'
